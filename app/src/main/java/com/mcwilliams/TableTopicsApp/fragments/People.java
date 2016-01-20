f821c84d2f9b5de855af96a2da677006d80f578b
Joshua McWilliams <jtmacnb@gmail.com>
2016-01-20 16:56:17 -0600

    Swipe to dismiss for recyclerview

--- app/src/main/java/com/mcwilliams/TableTopicsApp/fragments/People.java
+++ app/src/main/java/com/mcwilliams/TableTopicsApp/fragments/People.java
@@ -9,12 +9,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mcwilliams.TableTopicsApp.R;
 import com.mcwilliams.TableTopicsApp.TableTopicsApplication;
@@ -23,6 +25,7 @@ import com.mcwilliams.TableTopicsApp.arrayadapters.TopicsRvAdapter;
 import com.mcwilliams.TableTopicsApp.model.Member;
 import com.mcwilliams.TableTopicsApp.utils.DatabaseHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -33,6 +36,9 @@ import butterknife.ButterKnife;
  */
 public class People extends Fragment {
     public static RecyclerView peopleRV;
+    private ItemTouchHelper.SimpleCallback simpleCallback;
+    private ItemTouchHelper itemTouchHelper;
+    private static List<Member> memberList = new ArrayList<>();
 
     @Nullable
     @Override
@@ -46,17 +52,35 @@ public class People extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        peopleRV.setAdapter(new PeopleRVAdapter(TableTopicsApplication.db.getAllMembers()));
+        memberList = TableTopicsApplication.db.getAllMembers();
+        peopleRV.setAdapter(new PeopleRVAdapter(memberList));
     }
 
     public void setupRecyclerView(View view) {
         peopleRV.setHasFixedSize(true);
         peopleRV.setLayoutManager(new LinearLayoutManager(getActivity()));
+        simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                Member member = memberList.get(position);
+                TableTopicsApplication.db.deleteMember(member);
+                Toast.makeText(getContext(), member.get_name() + " deleted", Toast.LENGTH_SHORT).show();
+            }
+        };
+        itemTouchHelper =  new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(peopleRV);
     }
 
     public static void reloadData(){
         Log.d("","reloaded");
-        peopleRV.setAdapter(new PeopleRVAdapter(TableTopicsApplication.db.getAllMembers()));
+        memberList = TableTopicsApplication.db.getAllMembers();
+        peopleRV.setAdapter(new PeopleRVAdapter(memberList));
     }
 
     public void hideKeyboard(boolean showKeyboard, View view) {
